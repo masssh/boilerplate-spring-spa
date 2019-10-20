c029a8f2ef3a79daef1a65131f9982bec0816f79
masssh <utm.ver.2.0@gmail.com>
2019-10-20 14:47:34 +0900

    Initial commit

---
+++ src/test/java/masssh/boilerplate/spring/cli/BoilerplateSpringCliApplicationTests.java
@@ -0,0 +1,13 @@
+package masssh.boilerplate.spring.cli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoilerplateSpringCliApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
