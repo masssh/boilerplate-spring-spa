1c54d4ae7badc57d252825ecdc41f189f62be5cf
masssh <utm.ver.2.0@gmail.com>
2019-11-10 10:31:44 +0900

    Fix RedisConfiguration

--- web/src/main/java/masssh/boilerplate/spring/spa/web/config/RedisConfiguration.java
+++ web/src/main/java/masssh/boilerplate/spring/spa/web/config/RedisConfiguration.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import masssh.boilerplate.spring.spa.property.ApplicationProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.elasticsearch.config.ElasticsearchConfigurationSupport;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -12,7 +11,7 @@ import org.springframework.data.redis.core.RedisTemplate;
 @Configuration
 @RequiredArgsConstructor
 @SuppressWarnings("ALL")
-public class RedisConfiguration extends ElasticsearchConfigurationSupport {
+public class RedisConfiguration {
     private final ApplicationProperty applicationProperty;
 
     @Bean
