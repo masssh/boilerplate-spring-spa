83580fb9d2aec01c54477da8fe2c8305dcbe3c1b
Masahiro Yamashita <utm.ver.2.0@gmail.com>
2020-03-20 00:54:41 +0900

    Remove some local only endpoints

--- api/src/main/java/masssh/boilerplate/spring/spa/api/controller/LocalOnlyController.java
+++ api/src/main/java/masssh/boilerplate/spring/spa/api/controller/LocalOnlyController.java
@@ -8,23 +8,16 @@ import lombok.extern.slf4j.Slf4j;
 import masssh.boilerplate.spring.spa.api.service.MailService;
 import masssh.boilerplate.spring.spa.dao.service.UserCreator;
 import masssh.boilerplate.spring.spa.model.response.SuccessResponse;
-import masssh.boilerplate.spring.spa.model.row.UserRow;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.time.Instant;
-import java.util.Locale;
 import java.util.Map;
-import java.util.UUID;
-
-import static masssh.boilerplate.spring.spa.api.security.Roles.ROLE_USER;
 
 
 @Profile(value = "local")
@@ -55,37 +48,6 @@ public class LocalOnlyController {
     @GetMapping("/api/test")
     public ResponseEntity<SuccessResponse> apiTest() {
         return ResponseEntity.ok(new SuccessResponse());
-    }
-
-    @GetMapping("/local/user/add")
-    public ResponseEntity<Map<String, String>> addUser(final UserAddRequest request) {
-        final UserRow userRow = userCreator.tryCreate(
-                new UserRow(null,
-                        null,
-                        request.getName(),
-                        ROLE_USER,
-                        request.getEmail(),
-                        Locale.JAPAN.toLanguageTag(),
-                        passwordEncoder.encode(request.getPassword()),
-                        DigestUtils.md5DigestAsHex(UUID.randomUUID().toString().getBytes()),
-                        null));
-        return ResponseEntity.ok(Map.of("token", userRow.getAccessToken()));
-    }
-
-    @GetMapping("/local/user/add/random")
-    public ResponseEntity<Map<String, String>> addUserRandom() {
-        final String uid = String.valueOf(Instant.now().getEpochSecond());
-        final UserRow userRow = userCreator.tryCreate(
-                new UserRow(null,
-                        null,
-                        "user" + uid,
-                        ROLE_USER,
-                        uid + "@example.com",
-                        Locale.JAPAN.toLanguageTag(),
-                        passwordEncoder.encode("12345678"),
-                        DigestUtils.md5DigestAsHex(UUID.randomUUID().toString().getBytes()),
-                        null));
-        return ResponseEntity.ok(Map.of("token", userRow.getAccessToken()));
     }
 
     @GetMapping("/local/encode")
