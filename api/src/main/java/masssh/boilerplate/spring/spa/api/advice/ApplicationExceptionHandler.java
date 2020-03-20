71c4299e5e4e8525de59fe134ba6cfeaa8246d55
Masahiro Yamashita <utm.ver.2.0@gmail.com>
2020-03-21 01:47:11 +0900

    Clean ApplicationExceptionHandler

--- api/src/main/java/masssh/boilerplate/spring/spa/api/advice/ApplicationExceptionHandler.java
+++ api/src/main/java/masssh/boilerplate/spring/spa/api/advice/ApplicationExceptionHandler.java
@@ -1,10 +1,6 @@
 package masssh.boilerplate.spring.spa.api.advice;
 
-import static org.springframework.http.HttpStatus.BAD_REQUEST;
-import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
-
 import masssh.boilerplate.spring.spa.model.response.ErrorResponse;
-import org.eclipse.jetty.http.HttpHeader;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -18,6 +14,9 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+
 @RestControllerAdvice
 public class ApplicationExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler({Exception.class})
@@ -25,20 +24,22 @@ public class ApplicationExceptionHandler extends ResponseEntityExceptionHandler 
         final HttpStatus status;
         final int code;
         final String message;
+        final HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
 
         if (exception instanceof ResponseStatusException) {
             final ResponseStatusException responseStatusException = (ResponseStatusException) exception;
             status = responseStatusException.getStatus();
             code = status.value();
             message = StringUtils.isEmpty(exception.getMessage())
-                              ? status.getReasonPhrase()
-                              : exception.getMessage();
+                    ? status.getReasonPhrase()
+                    : exception.getMessage();
         } else if (exception instanceof IllegalArgumentException) {
             status = BAD_REQUEST;
             code = status.value();
             message = StringUtils.isEmpty(exception.getMessage())
-                              ? status.getReasonPhrase()
-                              : exception.getMessage();
+                    ? status.getReasonPhrase()
+                    : exception.getMessage();
         } else if (exception instanceof MethodArgumentNotValidException) {
             final BindingResult result = ((MethodArgumentNotValidException) exception).getBindingResult();
             status = BAD_REQUEST;
@@ -49,10 +50,6 @@ public class ApplicationExceptionHandler extends ResponseEntityExceptionHandler 
             code = status.value();
             message = exception.getMessage();
         }
-
-        final HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.add(HttpHeader.WWW_AUTHENTICATE.asString(), "Bearer realm=\"token_required\"");
         final ErrorResponse body = new ErrorResponse(code, message);
         return handleExceptionInternal(exception, body, headers, status, request);
     }
